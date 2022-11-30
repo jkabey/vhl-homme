6f0894c8874cded9cb7f72b93774c4077d39c3bd
jkabey <114315184+jkabey@users.noreply.github.com>
2022-11-29 19:06:43 -0800

    Update artifact.js

--- Artifact04/artifact.js
+++ Artifact04/artifact.js
@@ -1,6 +1,9 @@
-function validateform() {
-  var validFirstname = false;
-  var validLastname = false;
+function ValidateForm() {
+  var validUsername= false;
+  var validPassword= false;
+  var validFirstname= false;
+  var letters=/^[A-Za-z]+$/;
+  var numbers = /^[0=9]+$/;
   var validEmail = false;
   var validPhone = false;
   var validUsername = false;
@@ -10,121 +13,50 @@ function validateform() {
   var validState = false;
   var validCountry = false;
   var validZipcode = false;
-
-  var errorMessage = "";
-  var letters = /^[A-Za-z]+$/;
-  var numbers = /^[0=9]+$/;
-
-
-
-  //1) Create variable
-  //var validFirstname=false;
-  //2) read value from HTML
-  var firstname = document.getElementById("firstname").value;
-  //3) Do validation
+  var errorMessages="";
   if (myForm.firstname.value === null ||
     myForm.firstname.value === "" ||
     //value.match(letters)||
-    myForm.firstname.value.length > 20)
+    myForm.firstname.value.length > 20 ||
+    !myForm.firstname.value.match(letters))
     errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
   else
     validFirstname = true;
-  //4) Send error message to HTML
-  document.getElementById("errorMessages").innerHTML = "errorMessages";
-
-  //5) return status of each field
-  //return (validFirstname);
-
-  //1) Create variable
-  //var validLastname=false;
-  //2) read value from HTML
-  var validLastname = document.getElementById("lastname").value;
-
-  //3) Do validation
-  if (myForm.lastname.value === null ||
+   if (myForm.lastname.value === null ||
     myForm.lastname.value === "" ||
     myForm.lastname.value.length > 50)
     errorMessages += "<p>The lastname is required and cannot be greater than 50 characters</p>";
   else
-    validFirstname = true;
-  //4) Send error message to HMTL
-  document.getElementById("errorMessages").innerHTML = "errorMessages";
-  // Email//
-  var userEmail = document.getElementById("email").value;
-  var atpos = userEmail.indexOf("@");
-  var dotpos = userEmail.lastIndexOf(".");
-  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= userEmail.length)
-    // send error message.//  
-    errorMessages = "<p>Invalid email</p>";
+    validLastname = true;
+  if(myForm.username.value.length >12 ||
+     myForm.username.value === null ||
+     myForm.username.value === "" )
+    errorMessages += "<p> Please enter a valid username</p>";
   else
-    //return true; //Or assign the value to a variable. For example//
-    validEmail = true
-  document.getElementById("errorMessages").innerHTML = "errorMessages";
-  //5) return status of each field
-  // Phone //
-  var phone = document.getElementById("phone").value;
-  if (isNaN(phone) || phone.lenght > 15 || phone === null || phone === "");
-  //send error message. For example errorMessages = "<p>Invalid phone number </p>";
-  else
-    // return true; Or assign the value to a variable. For example//
-    validPhone = true;
-  //5) return status of each field
-
-  //You can also do pattern matching by using the match() method from the string object
-  //var numbers=/^[0-9]+$/;
-  //if (phone===null||phone ==="" ||phone.length>15|| //!phone.match(numbers))//
-  //...
-  //else//
-  //...
-
-  //-----Username-----//
-  if (myForm.username.value === null ||
-    myForm.username.value === "" ||
-    myForm.username.value.length > 12)
-    errorMessages += "<p> The Username must be less than 12 characters and is required</p>"
-  else
-    validUsername = true;
-  //5) return status of each field
-
-  //-----Password----//
+    //console.log(validUsername);
+  validUsername= true;
   if (myForm.password.value === null ||
     myForm.password.value === "" ||
+    //value.match(letters)||
     myForm.password.value.length > 7)
-    //|| value.match(numbers))
-    errorMessages += "<p> The Password must be less than 7 characters and is required</p>"
+    errorMessages += "<p>The Pasword is required and cannot be greater than 7 characters</p>";
   else
     validPassword = true;
-
-  //-----City------//
   if (myForm.city.value === null ||
     myForm.city.value === "")
     errorMessages += "<p> This field is required is required</p>"
   else
     validCity = true;
-  //5) return status of each field
-
-  //-----State----//
-  if (myForm.state.value === null ||
+    if (myForm.state.value === null ||
     myForm.state.value === "")
     errorMessages += "<p> This field is required</p>"
   else
     validState = true;
-  //5) return status of each field
-
-
-  //---------Country---------//
-  var country = document.getElementById("country").value;
   if (myForm.country.value === null ||
     myForm.country.value === "")
     errorMessages += "<p> This field is required</p>";
   else
     validCountry = true;
-  //5) return status of each field
-
-
-  //----  Zipcode---//
-  var zipcode = document.getElementById("zipcode").value;
-
   if (myForm.country.value === "USA" ||
     myForm.zipcode.value.lenght > 5)
     // validate Zipcode according to the rules
@@ -132,5 +64,23 @@ function validateform() {
   else
     //Zipcode is OK.  
     validZipcode = true;
-  return (validFirstname && validLastname);
+  var atpos = userEmail.indexOf("@");
+  var dotpos = userEmail.lastIndexOf(".");
+  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= userEmail.length)
+    // send error message.//  
+    errorMessages = "<p>Invalid email</p>";
+  else
+    //return true; //Or assign the value to a variable. For example//
+    validEmail = true;
+ if (myForm.phone.value === null ||
+    myForm.phone.value === "" ||
+    //value.match(letters)||
+    myForm.phone.value.length > 15 ||
+    !myForm.phone.value.match(numbers))
+    errorMessages += "<p>The Phone number is required </p>";
+  else
+    validPhone = true;
+  
+    (validUsername && validPassword && validFirstname && validEmail &&
+  validPhone && validUsername  && validPassword  && validAddress && validCity && validState && validCountry && validZipcode );
 }

e52b4d7c30c3057bf6f912a1834c7d68a8e16806
jkabey <114315184+jkabey@users.noreply.github.com>
2022-12-05 17:17:44 -0800

    Update scfbond.js

--- Module09/scfbond.js
+++ Module09/scfbond.js
@@ -18,3 +18,31 @@ event.stopPropagation();
     dot.style.top = (event.pageY - 4) + "px";
     document.body.appendChild(dot);
   });
+var dotColor = document.getElementById("dotColor");
+var dotSize = document.getElementById("dotSize");
+var mybutton = document.getElementById("clearButton");
+
+// Stop dots on inputs
+dotColor.addEventListener("click", function(event){
+    event.stopImmediatePropagation();
+});
+
+dotSize.addEventListener("click", function(event){
+    event.stopImmediatePropagation();
+});
+
+// Create Dots
+addEventListener("click", function(event) {
+    var dot = document.createElement("div");
+    dot.className = "dot";
+
+    dot.style.height = dotSize.value + "px";
+    dot.style.width = dotSize.value + "px";
+
+    dot.style.background = dotColor.value;
+
+    dot.style.left = (event.pageX) + "px";
+    dot.style.top = (event.pageY) + "px";
+
+    document.body.appendChild(dot);
+});

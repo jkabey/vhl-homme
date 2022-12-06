7abd4650d34f1c5fcbb6ee8b4f631fcaa953526f
jkabey <114315184+jkabey@users.noreply.github.com>
2022-12-05 16:32:58 -0800

    Update scfbond.js

--- Module09/scfbond.js
+++ Module09/scfbond.js
@@ -1,20 +1,39 @@
-var  mybutton = document.querySelector("button");
-mybutton.addEventListener("click", function(event) {
+var dotColor = document.getElementById("dotCol");
+var dotSize = document.getElementById("dotSize");
+var mybutton = document.getElementById("clearButton");
 
+// Stop dots on inputs
+dotColor.addEventListener("click", function(event){
+    event.stopImmediatePropagation();
+});
 
-var element = document.getElementsByTagName("div");
-for (index = element.length - 1; index >= 0; index--) {
-    element[index].parentNode.removeChild(element[index]);
-}
+dotSize.addEventListener("click", function(event){
+    event.stopImmediatePropagation();
+});
 
-// Let us stop the propagation of events
-
-event.stopPropagation();
-  });
-  addEventListener("click", function(event) {
+// Create Dots
+addEventListener("click", function(event) {
     var dot = document.createElement("div");
     dot.className = "dot";
-    dot.style.left = (event.pageX - 4) + "px";
-    dot.style.top = (event.pageY - 4) + "px";
+
+    dot.style.height = dotSize.value + "px";
+    dot.style.width = dotSize.value + "px";
+
+    dot.style.background = dotCol.value;
+
+    dot.style.left = (event.pageX) + "px";
+    dot.style.top = (event.pageY) + "px";
+
     document.body.appendChild(dot);
-  });
+});
+
+// Clear dots
+mybutton.addEventListener("click", function(event) {
+    var element = document.getElementsByTagName("div");
+    
+    for (index = element.length - 1; index >= 0; index--) {
+        element[index].parentNode.removeChild(element[index]);
+    }
+    event.stopPropagation();
+});
+
